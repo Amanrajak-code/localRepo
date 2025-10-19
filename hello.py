4d5338cfa125b0ff1c9ad3a2d4169a4e8de6aecf
Aman Raj <72144467+Amanrajak-code@users.noreply.github.com>
2025-10-19 19:02:15 +0530

    Update hello.py to include user input greeting

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-# this is my first program 
-# Only printing hello world
-print("Hello World")
+#Greeting you, Its my first program
+
+nam = input("Enter Your Name:")
+
+print(f'Hello {nam}. its my first hello World program')
+
